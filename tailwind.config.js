facd3d58296190249716ab11b8915437476918a9
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2021-09-15 12:37:59 -0400

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      blue: {
+        DEFAULT: "#3399cc",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
