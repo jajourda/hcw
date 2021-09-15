81adf25da8a3e2d73d65ea594d304378a1764d89
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2021-09-15 13:24:55 -0400

    update howchurch works domain google connection and link to old website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import _ from "lodash";
 import * as React from "react";
+import { getDataFromSheets } from "./api/sheets";
 
-export interface IHomeProps { }
-export default function Home() {
+
+export default function Home(props: any) {
+  console.log(props)
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -18,6 +20,31 @@ export default function Home() {
             How Church Works!
           </a>
         </h1>
+
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full p-6 mt-6 text-left border w-96 rounded-xl">
+          <h2 className="text-2xl">Coming Soon...</h2>
+          <ul>
+            {props.data && props.data.length ? (
+              props.data.map((item: any) => (
+                <li key={item.title}>
+                  {item.title} - {item.description}
+                </li>
+              ))
+            ) : (
+              <li>Error: do not forget to setup your env variables 👇</li>
+            )}
+          </ul>
+          <a
+            href="http://sonbuildmea.house"
+            className="p-6 mt-6 text-center border bg-green text-white w-96 rounded-xl hover:text-black focus:text-black"
+          >
+            <h3 className="text-3xl font-bold bg-bluen">Old Website &rarr;</h3>
+            {/* <p className="mt-4 text-xl">
+              some old items that will hopefully be reworked soon
+            </p> */}
+          </a>
+
+        </div>
 
 
 
@@ -41,4 +68,15 @@ export default function Home() {
       </footer>
     </div>
   );
+}
+
+export async function getStaticProps(context: any) {
+  let c = context;
+  const sheet = await getDataFromSheets();
+  return {
+    props: {
+      data: sheet.slice(1, sheet.length), // remove sheet header
+    },
+    revalidate: 1, // In seconds
+  };
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      blue: {
-        DEFAULT: "#3399cc",
+    extend: {
+      colors: {
+        blue: {
+          DEFAULT: "#3399cc",
+        },
+        green: {
+          DEFAULT: "#75b55e",
+        },
       },
     },
   },
