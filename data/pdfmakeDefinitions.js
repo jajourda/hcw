306d22bca264414a56ba8937679c4ee21581c93c
Joshua Andrew Jourdain <jajourda@users.noreply.github.com>
2021-09-22 17:05:03 -0400

    add react-pdfmake support

---
+++ data/pdfmakeDefinitions.js
@@ -0,0 +1,7 @@
+export const dd = {
+	content: [
+		'First paragraph',
+		'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+	]
+	
+}
