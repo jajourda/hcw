77d4ef5da23e2fd289b5de1bb7ebf0c5156ca1b0
Joshua Andrew Jourdain <jajourda@users.noreply.github.com>
2021-09-22 17:08:51 -0400

    remove the unused variable error via the interace

--- components/MishnahMethod.tsx
+++ components/MishnahMethod.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 export interface IMishnahMethodProps {
 }
+  /* tslint:disable:no-unused-variable */
 
 export default function MishnahMethod (props: IMishnahMethodProps) {
   return (
