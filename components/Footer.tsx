17fc68123af4cb5b3500f6b504708fb37db85f6d
Joshua Andrew Jourdain <jajourda@users.noreply.github.com>
2021-10-21 17:00:11 -0400

    add youtube link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -81,8 +81,8 @@ export default function Footer () {
       </div>
       <div className="w-full md:w-4/12 px-4 mx-auto text-center">
         <div className="text-sm text-blueGray-500 font-semibold py-1">
-          Copyright © <span id="get-current-year">2021</span><a href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 hover:text-gray-800" target="_blank"> Notus JS by</a>
-          <a href="https://www.creative-tim.com?ref=njs-profile" className="text-blueGray-500 hover:text-blueGray-800">Creative Tim</a>.
+          Copyright © <span id="get-current-year">2021</span>
+          <a href="https://www.youtube.com/channel/UCGp8pFe132gzI6YvnAxHOXw" className="text-blueGray-500 hover:text-blueGray-800"> How Church Works</a>.
         </div>
       </div>
     </div>
