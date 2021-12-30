7090f960b406859493aeda7322bbaf6cc1859cd2
Joshua Andrew Jourdain <jajourda@users.noreply.github.com>
2021-12-30 16:07:13 -0500

    add loading spinner

--- pages/bibleinbitsvideo/[youtube].tsx
+++ pages/bibleinbitsvideo/[youtube].tsx
@@ -1,47 +1,55 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { useRouter } from 'next/router'
-import useSWR from 'swr'
-import BibVideoPost from '../../components/BibVideoPost';
-import Layout from '../../components/Layout';
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import BibVideoPost from "../../components/BibVideoPost";
+import Layout from "../../components/Layout";
+import Loading from "../../components/Loading";
 import Head from "next/head";
 
-const fetcher = async (url:string) => {
-  const res = await fetch(url)
-  const data = await res.json()
-  console.log('res')
-  console.log(res)
+const fetcher = async (url: string) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log("res");
+  console.log(res);
 
   if (res.status !== 200) {
-    throw new Error(data.message)
+    throw new Error(data.message);
   }
-  return data
-}
+  return data;
+};
 
-export interface IBibVideoPageProps {
-}
+export interface IBibVideoPageProps {}
 
-export default function BibVideoPage (props: IBibVideoPageProps) {
-    console.log(props)
+export default function BibVideoPage(props: IBibVideoPageProps) {
+  console.log(props);
 
-    const { query } = useRouter()
+  const { query } = useRouter();
   const { data, error } = useSWR(
     () => query.youtube && `/api/bib/${query.youtube}`,
     fetcher
-  )
+  );
 
-  if (error) return <div>{error.message}</div>
-  if (!data) return <div>Loading bibvid...</div>
+  if (error) return <div>{error.message}</div>;
+  if (!data)
+    return (
+      <Layout>
+        <Head>
+          <title>Bible-in-Bits | How Church Works</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Loading/>
+      </Layout>
+    );
 
   return (
     <Layout>
-    <Head>
-      <title>Bible-in-Bits | How Church Works</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    
-      
-      <BibVideoPost video={data}/>
+      <Head>
+        <title>Bible-in-Bits | How Church Works</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <BibVideoPost video={data} />
     </Layout>
   );
 }
