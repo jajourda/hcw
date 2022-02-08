17ceb5c95155b2c730ef473d10fc189b968b7c0c
Joshua Jourdain <yahwehprovides@Joshuas-iMac.local>
2022-02-08 06:52:09 -0500

    test unique disqus after inserting template string into public reading pages

--- components/DisqusComments.tsx
+++ components/DisqusComments.tsx
@@ -6,12 +6,13 @@ export interface IDisqusCommentsProps {
     identifier: string;
     title: string;
 }
-
+//>this comment helped to set up disqus so that each public reading page got its own unique disqus comments section
+//https://stackoverflow.com/questions/64021373/the-same-comments-under-different-posts
 export default function DisqusComments(props: IDisqusCommentsProps) {
     const disqusShortname = "how-church-works"
     const disqusConfig = {
-        url: props.url,
-        identifier: props.identifier, // Single post id
+        url: `${props.url}/${props.identifier}`,
+        identifier: `#${props.identifier}`, // Single post id
         title: props.title // Single post title
     }
     return (
